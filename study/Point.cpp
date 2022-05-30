bad664cd9a6b8d193678f967fb272c7fbdcd1724
YuLim2 <jyulim0120@gmail.com>
2022-05-30 10:15:50 +0900

    Unary operator overload

--- study/Point.cpp
+++ study/Point.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-#define ID_LEN 20
-#define MAX_SPD 200
-#define FUEL_STEP 2
-#define ACC_STEP 10
-#define BRK_STEP 10
-
-class Car
+class Point
 {
-    private:
-	    char gamerID[ID_LEN];
-	    int fuelGauge;
-	    int curSpeed;
-	public:
-	    void InitMembers(char* ID, int fuel, int speed);
-	    void ShowCarState();
-	    void Accel();
-	    void Break();
+private:
+	int xpos, ypos;
+public:
+	Point(int x, int y): xpos(x), ypos(y) {}
+	void ShowPosition() const
+	{
+		cout << '[' << xpos << ", " << ypos << ']' << endl;
+	}
+	Point operator*(int n){
+		Point res(xpos*n, ypos*n)
+		return res;
+	}
 };
-
-void Car::InitMembers(char* ID, int fuel, int speed)
-{
-	strcpy(gamerID, ID);
-	fuelGauge = fuel;
-	curSpeed = speed;
-}
 
 int main(void)
 {
-    
-	return 0;
+	Point p1(1, 2);
+	Point p2(3, 4);
+	Point p3 = p1.AddMemberFunc(p2);
+	p3.ShowPosition();
+	return 0;	
 }
