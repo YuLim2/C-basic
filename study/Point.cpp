ce3b0d9b6a44298142ed8cf235c5c1d8d685a84a
YuLim2 <jyulim0120@gmail.com>
2022-05-30 10:16:04 +0900

    ADD: Unary operator overload

--- study/Point.cpp
+++ study/Point.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+Point operator*(int& n, Point& p){
+		Point res(n*p.xpos, n*p.ypos)
+		return res;
+	}
+
 class Point
 {
 private:
@@ -11,17 +16,31 @@ public:
 	{
 		cout << '[' << xpos << ", " << ypos << ']' << endl;
 	}
-	Point operator*(int n){
+	Point operator*(int& n){
 		Point res(xpos*n, ypos*n)
 		return res;
 	}
-};
+	friend Point operator*(const int&, const Point&)
+}; 
 
 int main(void)
 {
-	Point p1(1, 2);
-	Point p2(3, 4);
-	Point p3 = p1.AddMemberFunc(p2);
-	p3.ShowPosition();
-	return 0;	
+    Point p1(1, 2);
+    Point p2(3, 4);
+    
+    Point p3 = p1*p2;
+    p3.ShowPosition();
+    
+    Point p4 = p1*5;
+    p4.ShowPosition();
+    
+    Point p5 = 5*p1;
+    p5.ShowPosition();
+    
+    if(p4==p5)
+        cout << "p4 == p5" << endl;
+    if(p4!=p5)
+        cout << "p4 != p5" << endl;
+    
+    return 0;   
 }
